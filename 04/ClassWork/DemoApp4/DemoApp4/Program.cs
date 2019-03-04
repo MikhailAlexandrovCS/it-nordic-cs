7a32ed6327dfdf5e1fe028c5712efd96c07c1837
Mikhail Alexandrov <mishaalexandrov2@yandex.ru>
2019-03-04 21:43:32 +0300

    ClassWork of the 4th lesson added

---
+++ 04/ClassWork/DemoApp4/DemoApp4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DemoApp4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите первое число: ");
+			int first = int.Parse(Console.ReadLine());
+			Console.Write("Введите второе число: ");
+			int second = int.Parse(Console.ReadLine());
+			Console.WriteLine("\n" + first.ToString() + " больше " + second.ToString() + ":" + (first > second));
+			Console.WriteLine(second.ToString() + " больше " + first.ToString() + ":" + (second > first));
+			Console.WriteLine(first.ToString() + " больше или равна " + second.ToString() + ":" + (first >= second));
+			Console.WriteLine(second.ToString() + " больше или равна " + first.ToString() + ":" + (second >= first));
+			Console.WriteLine(first.ToString() + " меньше или равна " + second.ToString() + ":" + (first <= second));
+			Console.WriteLine(second.ToString() + " меньше или равна " + first.ToString() + ":" + (second <= first));
+			//------------------------------
+			Console.Write("Введите число: ");
+			long number = long.Parse(Console.ReadLine());
+			Console.WriteLine(number.GetType().ToString());
+			if (number <= int.MaxValue && number >= int.MinValue)
+				Console.WriteLine((int)number + " " + number.GetType().ToString());
+			else
+				Console.WriteLine("Значение превосходит максимально возможное");
+			//-------------
+			Console.Write("Введите а: ");
+			double a = double.Parse(Console.ReadLine());
+			Console.Write("Введите h: ");
+			double h = double.Parse(Console.ReadLine());
+			Console.WriteLine("S боковое грани: " + Math.Round(((double)3 * a * h), 2).ToString());
+			Console.WriteLine("S полной поверхности: " + Math.Round(((3 / 2) * a * (a * Math.Sqrt(3) + 2 * h)), 3).ToString());
+			Console.WriteLine("V объем пирамиды: " + Math.Round(Math.Pow(a, 2) / 2 * (h / Math.Sqrt(2)) * Math.Sqrt(3), 3).ToString());
+			Console.ReadKey();
+		}
+	}
+}
