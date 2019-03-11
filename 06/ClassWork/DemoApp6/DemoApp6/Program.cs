d11e60de8ca5acaeba79e869ad460603ad5fc92d
MikhailAlexnadrovCS <mishaalexandrov2@yandex.ru>
2019-03-12 01:51:40 +0300

    ClassWork of the 6th lesson updated

--- 06/ClassWork/DemoApp6/DemoApp6/Program.cs
+++ 06/ClassWork/DemoApp6/DemoApp6/Program.cs
@@ -6,63 +6,66 @@ namespace DemoApp6
 	{
 		static void Main(string[] args)
 		{
-			string tmp;
-			do
-			{
-				Console.Write("Введите слово: ");
-				tmp = Console.ReadLine();
-				if (tmp == "exit")
-					break;
-				if (tmp.Length <= 15)
-					Console.WriteLine("Длина строки: " + tmp.Length);
-				else
-				{
-					Console.WriteLine("Слишком длинная строка. Попробуйте другую.");
-					continue;
-				}
-			}
-			//----------------------------------------------------------
-			while (true) ;
-			int[] numbers = { 4, 8, 1, 4, 8, 3, 4 };
-			int sum = 0;
-			int iteration = 0;
-			while (true)
-			{
-				if (iteration == numbers.Length)
-					break;
-				sum += numbers[iteration];
-				Console.WriteLine("Промежуточная сумма: " + sum);
-				iteration++;
-			}
-			//----------------------------------------------------------
-			var marks = new[]
+            string tmp;
+            do
+            {
+                Console.Write("Введите слово: ");
+                tmp = Console.ReadLine();
+                if (tmp == "exit")
+                    break;
+                if (tmp.Length <= 15)
+                    Console.WriteLine("Длина строки: " + tmp.Length);
+                else
+                {
+                    Console.WriteLine("Слишком длинная строка. Попробуйте другую.");
+                    continue;
+                }
+            }
+            while (true);
+            //----------------------------------------------------------
+            int[] numbers = { 4, 8, 1, 4, 8, 3, 4 };
+            int sum = 0;
+            int iteration = 0;
+            while (true)
+            {
+                if (iteration == numbers.Length)
+                    break;
+                sum += numbers[iteration];
+                Console.WriteLine("Промежуточная сумма: " + sum);
+                iteration++;
+            }
+            //----------------------------------------------------------
+            var marks = new[]
 			{
 				new[] {2, 3, 3, 2, 3},
 				new[] {2, 4, 5, 3},
-				new[] {4 },
+				null,
 				new[] {5, 5, 5, 5},
 				new[] {4}
 			};
 			double avgValue = 0;
 			double avgDay = 0;
-			for (int i = 0; i < marks.GetLength(0); i++)
+            double del = 0;
+			for (int i = 0; i < marks.Length; i++)
 			{
 				try
 				{
-					for (int j = 0; j < marks.GetLength(1); j++)
+					for (int j = 0; j < marks[i].Length; j++)
 					{
 						avgValue += marks[i][j];
-						avgDay += avgDay;
-						Console.WriteLine("Средний балл за день " + (i + 1).ToString() +": " + (avgValue / marks.GetLength(1)));
+                        del = marks[i].Length;
 					}
-				}
-				catch(IndexOutOfRangeException e)
+                    avgDay += avgValue / del;
+                    Console.WriteLine("Средний балл за день " + (i + 1).ToString() + ": " + (avgValue / del).ToString());
+                    avgValue = 0;
+                }
+				catch(NullReferenceException e)
 				{
 					Console.WriteLine("Средний балл за день " + (i + 1).ToString() + ": N/A");
 					continue;
 				}
 			}
-			Console.WriteLine("Средний балл за неделю: " + (avgDay / marks.GetLength(0)));
+			Console.WriteLine("Средний балл за неделю: " + (avgDay / marks.Length));
 			Console.ReadKey();
 		}
 	}
