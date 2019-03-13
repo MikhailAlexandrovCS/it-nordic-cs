4d8ed6ab1d1eaf7dd5e6e30892a46300c33279ff
Mikhail Alexandrov <mishaalexandrov2@yandex.ru>
2019-03-13 21:38:59 +0300

    ClassWork of the 7th lesson

---
+++ 07/ClassWork/DemoApp7/DemoApp7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DemoApp7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double first = double.Parse(Console.ReadLine());
+			double second = double.Parse(Console.ReadLine());
+
+			Console.WriteLine(first + " * " + second + " = " + (first * second));
+			Console.WriteLine(String.Format("{0:##.##} + {1:##.##} = {2:##.##}", first, second, (first + second)));
+			Console.WriteLine($"{first:##.##} - {second:##.##} = {(first - second):##.##}");
+			//------------------------------------------------------------------------------------------------
+			string test = "my string test";
+			Console.WriteLine(test.Substring(0, test.Length));
+			//------------------------------------------------------------------------------------------------
+			Console.ReadKey();
+		}
+	}
+}
