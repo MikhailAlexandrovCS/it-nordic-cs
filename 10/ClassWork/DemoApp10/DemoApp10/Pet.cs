ebbf4475125428d4828221f66f5736947ca96192
MikhailAlexandrovCS <mishaalexandrov2@yandex.ru>
2019-03-25 21:30:02 +0300

    ClassWork of the 10th lesson added

---
+++ 10/ClassWork/DemoApp10/DemoApp10/Pet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoApp10
+{
+	class Pet
+	{
+		public string Kind;
+		public string Name;
+		public char Sex;
+		public int Age;
+
+		public string PropertyString
+		{
+			get
+			{
+				return $"{Name} id a {Kind} ({Sex}) of {Age} years old";
+			}
+		}
+		
+		public void WritePropertyToConsole()
+		{
+			Console.Write(PropertyString);
+		}
+	}
+}
---
+++ 10/ClassWork/DemoApp10/DemoApp10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DemoApp10
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pet pet = new Pet
+			{
+				Kind = "Dog",
+				Name = "Ryzhik",
+				Sex = 'M',
+				Age = 11
+			};
+			pet.WritePropertyToConsole();
+			Console.WriteLine();
+			Pet pet2 = new Pet();
+			pet2.Kind = "Cat";
+			pet2.Name = "Boris";
+			pet2.Sex = 'M';
+			pet2.Age = 9;
+			pet2.WritePropertyToConsole();
+			Console.ReadKey();
+		}
+	}
+}
