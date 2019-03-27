27b768a6efab26a422281ceb1ff68c753ef3004e
MikhailAlexandrovCS <mishaalexandrov2@yandex.ru>
2019-03-27 21:42:41 +0300

    ClassWork of the 11th lesson added

---
+++ 11/ClassWork/DemoApp11/DemoApp11/Pet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoApp11
+{
+	class Pet
+	{
+		public string Kind;
+		public string Name;
+		public char Sex;
+		public DateTimeOffset DateOfBirth;
+
+		public string Description
+		{
+			get
+			{
+				return $"{Name} id a {Kind} ({Sex}) of {Age} years old";
+			}
+		}
+
+		public int Age
+		{
+			get
+			{
+				TimeSpan age = DateTimeOffset.Now.Subtract(DateOfBirth);
+				return Convert.ToInt32(Math.Floor(age.TotalDays / 365.242));
+			}
+		}
+
+		public void WritePropertyToConsole(bool isShortDescription)
+		{
+			Console.Write(isShortDescription ? ShortDescription : Description);
+		}
+
+		public string ShortDescription
+		{
+			get
+			{
+				return $"{Name} id a {Kind}";
+			}
+		}
+	}
+}
---
+++ 11/ClassWork/DemoApp11/DemoApp11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DemoApp11
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pet pet = new Pet
+			{
+				Kind = "Dog",
+				Name = "Ryzhik",
+				Sex = 'M',
+				DateOfBirth = DateTimeOffset.Parse("2011-03-12")
+			};
+			pet.WritePropertyToConsole(false);
+			Console.WriteLine();
+			Pet pet2 = new Pet();
+			pet2.Kind = "Cat";
+			pet2.Name = "Boris";
+			pet2.Sex = 'M';
+			pet2.DateOfBirth = DateTimeOffset.Parse("2010-10-12");
+			pet2.WritePropertyToConsole(true);
+			Console.ReadKey();
+		}
+	}
+}
