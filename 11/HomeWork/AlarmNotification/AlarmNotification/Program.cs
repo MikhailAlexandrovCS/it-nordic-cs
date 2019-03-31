acdc66f6afda580fa7852642939fd9544e8afc84
MikhailAlexnadrovCS <mishaalexandrov2@yandex.ru>
2019-03-31 22:42:50 +0300

    HomeWork of the 11th lesson added

---
+++ 11/HomeWork/AlarmNotification/AlarmNotification/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace AlarmNotification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReminderItem firstAlarm = new ReminderItem(DateTimeOffset.Parse("01/04/2019 10:00", new CultureInfo("ru-RU").DateTimeFormat),
+                "Пора вставать!");
+            firstAlarm.WriteProperties();
+
+            ReminderItem secondAlarm = new ReminderItem(DateTimeOffset.Parse("31/03/2019 17:31", new CultureInfo("ru-RU").DateTimeFormat),
+                "Дневной сон окончен!");
+            secondAlarm.WriteProperties();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 11/HomeWork/AlarmNotification/AlarmNotification/ReminderItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlarmNotification
+{
+    class ReminderItem
+    {
+        private DateTimeOffset AlarmDate { get; set; }
+        private string AlarmMessage { get; set; }
+        
+        public TimeSpan TimeToAlarm
+        {
+            get
+            {
+                return DateTimeOffset.Now.Subtract(AlarmDate);
+            }
+        }
+
+        public bool isOutdated
+        {
+            get
+            {
+                return TimeToAlarm.Milliseconds >= 0 ? true : false;
+            }
+        }
+
+        public ReminderItem(DateTimeOffset alarmDate, string alarmMessage)
+        {
+            AlarmDate = alarmDate;
+            AlarmMessage = alarmMessage;
+        }
+        
+        public void WriteProperties()
+        {
+            Console.Write($"Время и дата будильника: {AlarmDate.ToString("dd/MM/yyyy HH:mm")}\n" +
+                $"Сообщение будильника: {AlarmMessage}\n" +
+                $"Время до срабатывания будильника: " +
+                $"{(!isOutdated ? $"{Math.Abs(TimeToAlarm.Hours)}:{Math.Abs(TimeToAlarm.Minutes)}:{Math.Abs(TimeToAlarm.Seconds)}" : "00:00:00")}\n" +
+                $"Статус будильника: {(isOutdated ? "Просрочен" : "Не просрочен")}\n");
+        }
+    }
+}
