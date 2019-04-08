38c92a0628d4832682bd24b6d5690380e1497a5e
MikhailAlexandrovCS <mishaalexandrov2@yandex.ru>
2019-04-08 21:29:17 +0300

    ClassWork of the 14th lesson addded

---
+++ 14/ClassWork/DemoApp14/DemoApp14/ErrorList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoApp14
+{
+	class ErrorList : IDisposable, IEnumerable<string>
+	{
+		public string Category;
+		private List<string> _errors;
+		public static string OutputPrefixFormat;
+
+		public static string OutputPrefix { get; set; }
+
+		static ErrorList()
+		{
+			OutputPrefixFormat = "HH:mm:ss";
+		}
+
+		public void WriteToConsole()
+		{
+			foreach (var item in _errors)
+				Console.WriteLine(DateTimeOffset.Now.ToString(OutputPrefixFormat) + ": " + Category + ": "
+					+ " " + item);
+		}
+
+		public void Add(string errorMessage)
+		{
+			_errors.Add(errorMessage);
+		}
+
+		public ErrorList(string category)
+		{
+			Category = category;
+			_errors = new List<string>();
+		}
+
+		public void Dispose()
+		{
+			if (_errors != null)
+			{
+				_errors.Clear();
+				_errors = null;
+			}
+		}
+
+		public IEnumerator<string> GetEnumerator()
+		{
+			return ((IEnumerable<string>)_errors).GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return ((IEnumerable<string>)_errors).GetEnumerator();
+		}
+	}
+}
---
+++ 14/ClassWork/DemoApp14/DemoApp14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DemoApp14
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (ErrorList errorList = new ErrorList("Error"))
+			{
+				errorList.Add("Неправильный формат");
+				errorList.Add("Неправильный лог");
+				errorList.Add("Неправильный знак");
+				errorList.WriteToConsole();
+				//foreach (var error in errorList)
+				//	Console.WriteLine(errorList.Category + $": {error}");
+			}
+			Console.ReadKey();
+		}
+	}
+}
